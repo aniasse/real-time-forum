5b8ec69e26c231a87f635d557f6b0aa890e0a775
abdouladieng <diengabdoulaziz110@gmail.com>
2024-01-26 17:27:28 +0000

    index and database

--- database/forums.sql
+++ database/forums.sql
@@ -1,12 +1,5 @@
-
---Creation de la base de donnée
---CREATE DATABASE IF NOT EXISTS db;
-
---Utilisation de la base de donnée
---USE db;
-
--- Creation d'une table "utilisateur"
-CREATE TABLE IF NOT EXISTS users(
+-- Table des utilisateurs
+CREATE TABLE IF NOT EXISTS Users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nickname TEXT NOT NULL,
     firstname TEXT NOT NULL,
@@ -14,5 +7,30 @@ CREATE TABLE IF NOT EXISTS users(
     email TEXT NOT NULL,
     gender TEXT NOT NULL,
     age INTEGER NOT NULL,
+    session TEXT NOT NULL,
     password VARCHAR(254) NOT NULL
-)
+);
+
+-- Table des publications (Post)
+CREATE TABLE IF NOT EXISTS Post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    title VARCHAR(254) NULL,
+    content VARCHAR(254) NULL,
+    image VARCHAR(254) NULL,
+    date_created TIMESTAMP NULL
+);
+
+-- Table de liaison entre les publications et les catégories (Post_Category)
+CREATE TABLE IF NOT EXISTS Post_Category (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cat_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL
+);
+
+-- Table des sessions
+CREATE TABLE IF NOT EXISTS Session (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email VARCHAR(250),
+    date_fin TIMESTAMP
+);
