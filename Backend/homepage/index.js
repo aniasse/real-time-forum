9221c05b3ce12fb0895fac23d662a320109481da
abdouladieng <diengabdoulaziz110@gmail.com>
2024-02-22 15:14:02 +0000

    updating homepage

--- Backend/homepage/index.js
+++ Backend/homepage/index.js
@@ -28,3 +28,40 @@ nav.addEventListener("mouseleave", () => {
 });
 
 //Create Posts
+const create = document.querySelector('.button-create');
+const createPost = document.querySelector('.createpost');
+const closePostBox = document.querySelector('.close')
+const seeComs = document.querySelectorAll('.comment')
+const coms = document.querySelectorAll('.comments')
+
+create.addEventListener("click", () => {
+    if (createPost.style.display === 'none' || createPost.style.display === '' ) createPost.style.display = 'flex'
+    else createPost.style.display = 'none';
+})
+
+closePostBox.addEventListener('click', () => {
+    if (createPost.style.display !== 'none') createPost.style.display = 'none'
+})
+
+seeComs.forEach((seeCom, i) => {
+    seeCom.addEventListener('click', () => {
+        if (coms[i].style.display === 'none' || coms[i].style.display === '') coms[i].style.display = 'flex';
+        else coms[i].style.display = 'none';
+    });
+});
+
+//Messages
+
+const messages = document.querySelectorAll('.messages .usr')
+
+const discus = document.querySelectorAll('.discus')
+
+console.log(messages);
+console.log(discus);
+
+messages.forEach((mes, i) => {
+    mes.addEventListener('click', () => {
+        if (discus[i].style.display === 'none' || discus[i].style.display === '') discus[i].style.display = 'flex';
+        else discus[i].style.display = 'none';
+    })
+})
