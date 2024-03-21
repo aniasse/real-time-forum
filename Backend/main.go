ba17d8ccfe2de38818d0be144b3854897e2e670e
abdouladieng <diengabdoulaziz110@gmail.com>
2024-03-21 14:51:18 +0000

    delete coms

--- Backend/main.go
+++ Backend/main.go
@@ -7,23 +7,9 @@ import (
 	"net/http"
 )
 
-// func main() {
-// 	// Initialiser la base de données
-// 	database.InitDB()
-
-// 	// Configurer le routeur de l'API
-// 	api.Router()
-// }
-
 func main() {
 	// Initialiser la base de données
 	database.InitDB()
-
-	// // Gestion du serveur de fichiers statiques
-	// http.HandleFunc("/static/CSS/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Header().Set("Content-Type", "text/css")
-	// 	http.ServeFile(w, r, r.URL.Path[1:])
-	// })
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	// Configurer le routeur de l'API
